2a513c22de843373021d042b875ef0ca9d9323af
Harmain Bashir <162583900+harmainbashir15@users.noreply.github.com>
2024-10-16 17:59:54 +0500

    Update script.js

--- milestone3/script.js
+++ milestone3/script.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', function (event) {
     var experience = document.getElementById('experience').value;
     var skill = document.getElementById('skills').value;
     //generate the resume content dynamically
-    var resumeHTML = "\n<h2><b>Resume</b></h2>\n<h3> Personal Information </h3>\n<p><b>Name:</b>".concat(name, "</p>\n<p><b>Phone:</b>").concat(phone, "</p>\n<p><b>Email:</b>").concat(email, "</p>\n\n<h3>Education</h3>\n<p>").concat(education, "</p>\n\n<h3>Experience</h3>\n<p>").concat(experience, "</p>\n\n<h3>Skills</h3>\n<p>").concat(skill, "</p>\n");
+    var resumeHTML = "\n<h2><b>Resume</b></h2>\n<h3> Personal Information </h3>\n<p><b>Name:</b>".concat(name, "</p>\n<p><b>Email:</b>").concat(email, "</p>\n<p><b>Phone:</b>").concat(phone, "</p>\n\n<h3>Education</h3>\n<p>").concat(education, "</p>\n\n<h3>Experience</h3>\n<p>").concat(experience, "</p>\n\n<h3>Skills</h3>\n<p>").concat(skill, "</p>\n");
     //display the generated resume
     if (resumeDisplayElement) {
         resumeDisplayElement.innerHTML = resumeHTML;
